63eb596bb6879d9e9203d34bd1106bd713e9be47
管仁阳 <695401454@qq.com>
2022-04-14 02:27:57 +0800

    信息上报页面跳转

---
+++ app/src/main/java/com/example/cdc/ui/SpecialConditionActivity.kt
@@ -0,0 +1,21 @@
+package com.example.cdc.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.cdc.R
+
+class SpecialConditionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_special_condition)
+
+        // 从特殊信息上报页面提交表单
+        val specialConditionSubmitButton : Button = findViewById(R.id.special_condition_submit_button)
+
+        specialConditionSubmitButton.setOnClickListener {
+            startActivity(Intent(this, SubmitSuccessMessageActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cdc/ui/SubmitSuccessMessageActivity.kt
@@ -0,0 +1,22 @@
+package com.example.cdc.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.cdc.MainActivity
+import com.example.cdc.R
+
+class SubmitSuccessMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit_success_message)
+
+        // 从特殊信息上报页面提交表单
+        val submitSuccessReturnButton : Button = findViewById(R.id.submit_success_return_button)
+
+        submitSuccessReturnButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
