a54bd2060cbe78cc711ebd1da8cfce03e3c1b74b
FlyingPolaris <polarisybc@qq.com>
2022-04-30 10:27:45 +0800

    问卷类

--- app/src/main/java/com/example/cdc/ui/dataProcessing/ViewQuestionnaireListActivity.kt
+++ app/src/main/java/com/example/cdc/ui/dataProcessing/ViewQuestionnaireListActivity.kt
@@ -3,6 +3,7 @@ package com.example.cdc.ui.dataProcessing
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.VibrationAttributes
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
@@ -12,6 +13,41 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cdc.R
+
+open class Question(date: String) {
+    var questionList = mutableListOf<String>("ID", "姓名", "体温")
+}
+
+class Questionnaire(date: String) : Question(date) {
+    var answerList = mutableListOf<String>("", "", "")
+    val safety: Boolean
+        get() = this.checkSafe()
+    val id: String
+        get() = this.getid()
+    val name: String
+        get() = this.getname()
+    fun getAttribute(attribute: String): String{
+        var attr = ""
+        for (i in questionList.indices){
+            if (questionList[i] == attribute)
+                attr = answerList[i]
+        }
+        return attr
+    }
+    private fun checkSafe(): Boolean{
+        var safety = true
+        val temperature = getAttribute("体温").toDoubleOrNull()
+        if(temperature == null || temperature >= 37.3)
+            safety = false
+        return safety
+    }
+    private fun getid(): String{
+        return getAttribute("ID")
+    }
+    private fun getname(): String{
+        return getAttribute("姓名")
+    }
+}
 
 
 class ViewQuestionnaireListActivity : AppCompatActivity() {
