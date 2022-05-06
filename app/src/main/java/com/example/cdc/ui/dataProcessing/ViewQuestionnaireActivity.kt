8115f83984f3c387ef467afd2bf3c59ea4edc882
chn0318 <1623499602@qq.com>
2022-05-06 15:11:55 +0800

    完善了问卷类相关的网络接口

--- app/src/main/java/com/example/cdc/ui/dataProcessing/ViewQuestionnaireActivity.kt
+++ app/src/main/java/com/example/cdc/ui/dataProcessing/ViewQuestionnaireActivity.kt
@@ -28,15 +28,14 @@ class ViewQuestionnaireActivity : AppCompatActivity() {
         .writeTimeout(10, TimeUnit.SECONDS)  //写超时，也就是请求超时
         .build();
     /*
-        user_post_info(map):用户通过该函数向服务器提交防疫问卷，输入参数为HushMap的形式。
-        注意：在HushMap中，一定需要一个key值为Date的条目，它用于绑定后端的数据库，其格式为：db_日期
+        user_post_info(map):用户通过该函数向服务器的最新一个防疫数据库提交防疫信息，输入参数为HushMap的形式。
+        注意：提交的信息将会存储在最新被管理层创建的数据库中
         eg: 利用user_post_info()接口向服务器中5月3号的问卷数据库上报用户信息
         val mMap = HashMap<String,String>()
-        mMap.put("name","ybc")
-        mMap.put("id","0011")
-        mMap.put("gender","male")
-        mMap.put("temperature","36.5")
-        mMap.put("Date","db_0503") //!!一定要有！！
+        mMap.put("name","yzy")
+        mMap.put("id","0002")
+        mMap.put("safety","1")
+        mMap.put("temperature","36.9")
         user_post_info(mMap)
     */
     fun user_post_info(mMap:HashMap<String,String>){
@@ -69,15 +68,15 @@ class ViewQuestionnaireActivity : AppCompatActivity() {
         })
     }
     /*
-    *  user_get_table(Date):用户通过调用该函数获得某一天的防疫问卷都有哪些具体属性，值得注意的是Date采用db_日期的形式;
+    *  user_get_table():用户通过调用该函数获得最新的防疫问卷都有哪些具体属性;
     *  返回值为一个String，格式如下: 问题1;问题2;问题3;
     *  可以使用kotlin中String类内嵌的split方法来解析该字符串
-    *  eg: user_get_table("db_0503")
-    *  收到的响应报文为: name;id;gender;temperature;
+    *  eg: user_get_table()
+    *  收到的响应报文为: name;id;temperature;safety;
     * */
-    fun user_get_table(Date: String){
+    fun user_get_table(){
         val request: Request = Request.Builder()
-            .url("$BASE_URL/user_get_table.php?Date=$Date")
+            .url("$BASE_URL/user_get_table.php")
             .build()
         val call:Call =client.newCall(request)
         call.enqueue(object :Callback{
@@ -99,14 +98,13 @@ class ViewQuestionnaireActivity : AppCompatActivity() {
         })
     }
     /*
-    * admin_post_table(qlist):管理员通过该接口来创建一个问卷数据库，并指定该数据库所具有的属性。
-    * 参数格式: qlist数据类型为Array<String>,它的第一个元素为问卷日期（同时也是数据库的名字），格式为"db_日期"
-    *         之后的每个元素代表着创建数据库所具有的属性
-    * eg: 为5月4日创建一个数据库，其中的问卷的内容有name、id、gender、temperature
-    * val testList = arrayOf<String>("db_0504", "name", "id", "gender", "temperature")
+    * admin_post_table(qlist):管理员通过该接口来创建一个最新的问卷数据库，指定该数据库所具有的属性，并更新后端的Header指针（Header指针指向最新创建的数据库）。
+    * 参数格式: qlist数据类型为MutableList<String>，其中的每一个元素代表了一个问卷属性（约定：每一种问卷都具有id、name、safety这三种属性）
+    * eg: 创建最新的一个数据库，其中的问卷的内容有name、id、gender、temperature
+    * val testList: MutableList<String> = mutableListOf<String>("name", "id", "gender", "temperature","safety")
     * admin_post_table(testList)
     * */
-    fun admin_post_table(qlist:Array<String>){
+    fun admin_post_table(qlist:MutableList<String>){
         var x = FormBody.Builder()
         var i:Int =1
         for (item in qlist){
@@ -129,6 +127,57 @@ class ViewQuestionnaireActivity : AppCompatActivity() {
                 val body:String? = response.body?.string()
                 runOnUiThread {
                     //todo 数据上传成功，在这里控制UI界面
+                }
+                Log.e("OkHttp","get response successfully :${body}")
+            }
+
+        })
+    }
+    /*
+    * admin_get_id() 管理员可以通过调用该接口获得最新数据库中每一个条目的id，name,safety这三个属性（约定：每一种问卷都具有id、name、safety这三种属性）
+    * 返回报文的格式是一个字符串，格式如下: id1;name1;safety1;\nid2;name2;safety2;\n
+    * 即每一行代表一个用户的三个属性，用”;“隔开,每个用户信息之间通过换行符”\n“隔开，你可以使用String.split()函数来解析报文
+    * */
+    fun admin_get_id(){
+        val request: Request = Request.Builder()
+            .url("${BASE_URL}/admin_get_id.php")
+            .build()
+        val call:Call = client.newCall(request)
+        call.enqueue(object :Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("OkHttp","get response onFailure :${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body:String? = response.body?.string()
+                runOnUiThread { //
+                    //todo 数据获取成功，在这里控制UI界面
+                }
+                Log.e("OkHttp","get response successfully :${body}")
+            }
+
+        })
+    }
+    /*
+    * admin_get_questionnaire(id) 管理员通过该接口来获取某一特定用户在最新数据库所填写的问卷内容
+    * 参数：id:String ————>用于指定要查询的用户身份
+    * eg: 查询最新数据库中id为0318的用户信息
+    * admin_get_questionnaire("0318")
+    * */
+    fun admin_get_questionnaire(id:String){
+        val request: Request = Request.Builder()
+            .url("${BASE_URL}/admin_get_questionnaire.php?Id=$id")
+            .build()
+        val call:Call = client.newCall(request)
+        call.enqueue(object :Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("OkHttp","get response onFailure :${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body:String? = response.body?.string()
+                runOnUiThread { //
+                    //todo 数据获取成功，在这里控制UI界面
                 }
                 Log.e("OkHttp","get response successfully :${body}")
             }
